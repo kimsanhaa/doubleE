62d89dea7d6ac80512d05a5405f341b16e93469e
sanha <ttksgk@gmail.com>
2022-12-25 22:12:50 +0900

    초기

---
+++ src/main/java/toy/project/doubleE/DoubleEApplication.java
@@ -0,0 +1,13 @@
+package toy.project.doubleE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleEApplication.class, args);
+	}
+
+}
