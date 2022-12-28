22b1aaedf3f8cd56e906e7b748c3374fe9477f07
sanha <ttksgk@gmail.com>
2022-12-28 22:14:11 +0900

    서버없이 페이지 이동 예제

--- src/main/java/toy/project/doubleE/defaultController.java
+++ src/main/java/toy/project/doubleE/defaultController.java
@@ -3,12 +3,27 @@ package toy.project.doubleE;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class defaultController {
-    //localhost:8080/
+    //localhost:8080/main
     @GetMapping("")
     public  String home(){
+
         return "login";
     }
+
+
+    //localhost:8080/main
+    @GetMapping("/main") //받는애 get방식으로
+    public String main(){return "main";}
+
+
+
+
+    @PostMapping("/main") //받는애 Post방식으로
+    @ResponseBody
+    public String postMain(){return "main";}
 }
