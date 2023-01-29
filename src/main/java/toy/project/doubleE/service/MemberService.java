766f2c8d67b7b8f0ce7cd2b3f5b2e499ccbdf5b7
sanha <ttksgk@gmail.com>
2023-01-29 21:02:59 +0900

    Feat : login Controller 추가 / loginCheck service 추가

--- src/main/java/toy/project/doubleE/service/MemberService.java
+++ src/main/java/toy/project/doubleE/service/MemberService.java
@@ -1,11 +1,18 @@
 package toy.project.doubleE.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import toy.project.doubleE.dto.memberDto;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 @Service
 public class MemberService {
 
+    @Autowired
+    HttpSession session;
+    ArrayList<memberDto> userList = new ArrayList<>();
 
 
     public String joinCheck(String name, String age, String id, String password){
@@ -14,6 +21,24 @@ public class MemberService {
         user.setAge(age);
         user.setId(id);
         user.setPassword(password);
+        userList.add(user);
         return "success";
     }
+
+    public String loginCheck(String id, String password){
+
+        for(int i=0; i< userList.size(); i++){
+
+        if(userList.get(i).getId().equals(id)){
+          if(userList.get(i).getPassword().equals(password)){
+              session.setAttribute("user",userList.get(i).getName());
+
+              System.out.println(session.getAttribute("user"));
+            return "ok";
+             } return "false";
+          }
+        }
+        return "false";
+    }
+
 }
